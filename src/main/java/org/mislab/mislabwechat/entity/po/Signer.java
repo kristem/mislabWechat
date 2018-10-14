ecefbad0e2f1dc0e6296bc0f52aa863f66c12dda
misa527528 <1353236065@qq.com>
2018-10-14 17:27:44 +0800

    #Other imples async method

--- src/main/java/org/mislab/mislabwechat/entity/po/Signer.java
+++ src/main/java/org/mislab/mislabwechat/entity/po/Signer.java
@@ -1,11 +1,13 @@
 package org.mislab.mislabwechat.entity.po;
 
+import lombok.Builder;
 import lombok.Data;
 
 /**
  * 把签到情况存到数据库中（签到的编号，学生学号，姓名）
  */
 @Data
+@Builder
 public class Signer {
     private String signId;
     private String studentName;
--- src/main/java/org/mislab/mislabwechat/manager/impl/SignInManagerImpl.java
+++ src/main/java/org/mislab/mislabwechat/manager/impl/SignInManagerImpl.java
@@ -1,15 +1,32 @@
 package org.mislab.mislabwechat.manager.impl;
 
+import org.mislab.mislabwechat.dao.SignerMapper;
+import org.mislab.mislabwechat.dao.UserMapper;
+import org.mislab.mislabwechat.entity.po.Signer;
+import org.mislab.mislabwechat.entity.po.User;
 import org.mislab.mislabwechat.manager.SignInManager;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class SignInManagerImpl implements SignInManager {
+	@Resource
+	private UserMapper userMapper;
+	@Resource
+	private SignerMapper signerMapper;
 
 	@Async
 	@Override
 	public void saveSignInMessage(String openId) {
+		// step1.根据DB获取该学生的基本信息
+		User user = userMapper.findUserByOpenId(openId);
 
+		// step2.将信息存储到签到表
+		if (user != null) {
+			Signer signer = Signer.builder().studentId(user.getStudentId()).studentName(user.getStudentName()).build();
+			signerMapper.insertSigner(signer);
+		}
 	}
 }
